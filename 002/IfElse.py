8ed69c2891666b71a77f2db511c4c557d42fb9a4
Ken Dong <yid164@mail.usask.ca>
2020-03-31 17:01:44 -0600

    002 is If and Else Condition

---
+++ 002/IfElse.py
@@ -0,0 +1,13 @@
+print('------------------ Made By Ken ------------------')
+
+temp = input("Guess My Number: ")
+
+guess = int(temp)
+
+if guess == 8:
+    print("You got it")
+    print("Thanks!")
+else:
+    print("Sorry, you got wrong answer")
+    
+print("Game Over")
